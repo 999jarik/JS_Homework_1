1d0531471c20463337ce2ef0e45d95956f128d08
Yaroslav Trofimenko <92098465+999jarik@users.noreply.github.com>
2023-04-08 02:09:00 +0300

    1 exercise done

---
+++ scripts/scripts_ex1.js
@@ -0,0 +1,25 @@
+function mathPow(a, b) {
+    var resultPow = (a ** b);
+    return resultPow
+}
+
+let num
+let degree
+
+while (isNaN(num)) {
+    num = +prompt("Введіть число:", 0);
+    console.log(typeof num, num)
+    if (isNaN(num)) {
+        alert('Це не число, введи число');
+    }
+}
+while (isNaN(degree)) {
+    degree = +prompt("Введіть степінь: ", 0 );
+    console.log(typeof degree, degree)
+    if (isNaN(degree)) {
+        alert('Це не число, введи число');
+    }
+}
+
+let numInPow = mathPow(num, degree);
+alert('число ' + num + ' в степені ' + degree + ' дорівнює ' + numInPow);
