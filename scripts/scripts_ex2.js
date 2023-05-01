6c33c03da08986eabab36e5386a0dda6b1003c09
Yaroslav Trofimenko <92098465+999jarik@users.noreply.github.com>
2023-05-01 21:18:52 +0300

    Update scripts_ex2.js

--- scripts/scripts_ex2.js
+++ scripts/scripts_ex2.js
@@ -2,10 +2,26 @@ let nameUsersArr = [];
 let check = true;
 
 for(let i = 1; i < 6; i++) {
-    nameUsersArr.push(prompt("Зареєструйте ім'я користувача № " + i));
-}
+    var nameUser = null;
+    while (nameUser === null || nameUser.trim() === "") {
+      nameUser = prompt("Зареєструйте ім'я користувача № " + i);
+      if (nameUser === null) {
+        break;
+      }
+      if (nameUser.trim() === "") {
+        alert("Введіть ім'я користувача перед тим, як продовжити.");
+      }
+    }
+    if (nameUser === null) {
+        break;
+      }
+    nameUsersArr.push(nameUser);
+  }
 
 while(check) {
+    if (nameUser === null) {
+        break;
+      }
     let nameLogin = prompt("Введіть ім'я для входу: ")
 
     for (let i = 0; i < nameUsersArr.length; i++) {
@@ -17,5 +33,5 @@ while(check) {
         }
     if(check === true) {
     alert("Імені не знайдено");
-    }
-}
+    };
+};
